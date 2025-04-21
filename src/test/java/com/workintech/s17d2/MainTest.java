4390dab69735acd5fbcd6f96fbdaf9bdb0564a02
yunus-emre-celik <yunus.celik.dev@gmail.com>
2025-04-21 17:26:14 +0300

    Done

--- src/test/java/com/workintech/s17d2/MainTest.java
+++ src/test/java/com/workintech/s17d2/MainTest.java
@@ -1,8 +1,8 @@
 package com.workintech.s17d2;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.workintech.s17d2.model.*;
 import com.workintech.s17d2.rest.DeveloperController;
+import com.workintech.s17d2.model.*;
 import com.workintech.s17d2.tax.DeveloperTax;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -48,7 +48,7 @@ class MainTest {
         double expectedSalary = 1000.0;
         Experience expectedExperience = Experience.JUNIOR;
         
-        Developer developer = new Developer(expectedId, expectedName, expectedSalary, expectedExperience);
+        Developer developer = new Developer(expectedExperience, expectedId, expectedName, expectedSalary);
 
         
         int actualId = developer.getId();
@@ -146,7 +146,7 @@ class MainTest {
         controller = new DeveloperController(new DeveloperTax());
         // Simulate @PostConstruct call if necessary. In reality, this is managed by Spring.
         controller.init();
-        Developer developer = new Developer(1, "Initial Developer", 5000.0, Experience.JUNIOR);
+        Developer developer = new Developer(Experience.JUNIOR, 1, "Initial Developer", 5000.0);
         mockMvc.perform(post("/developers")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(developer)))
@@ -199,7 +199,7 @@ class MainTest {
     @DisplayName("DeveloperController:AddDeveloper")
     @Order(2)
     void testAddDeveloper() throws Exception {
-        Developer newDeveloper = new Developer(2, "New Developer", 6000.0, Experience.MID);
+        Developer newDeveloper = new Developer(Experience.MID, 2, "New Developer", 6000.0);
         mockMvc.perform(post("/developers")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(newDeveloper)))
@@ -228,7 +228,7 @@ class MainTest {
     @DisplayName("DeveloperController:UpdateDeveloper")
     @Order(5)
     void testUpdateDeveloper() throws Exception {
-        Developer updatedDeveloper = new Developer(1, "Updated Developer", 7000.0, Experience.SENIOR);
+        Developer updatedDeveloper = new Developer(Experience.SENIOR, 1, "Updated Developer", 7000.0);
         mockMvc.perform(put("/developers/{id}", 1)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(updatedDeveloper)))
